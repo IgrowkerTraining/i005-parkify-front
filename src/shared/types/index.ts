36507776f262c7a4ec970202ee5c9d167d06315e
vlopez2828 <vickylopez2828@gmail.com>
2025-04-11 18:57:23 -0300

    chore: remove unused import

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,4 +1,4 @@
-import { FieldErrors, UseFormRegister, UseFormSetValue, UseFormTrigger } from "react-hook-form";
+import { FieldErrors, UseFormRegister, UseFormSetValue } from "react-hook-form";
 import { FormRegisterValues } from "../../features/auth/types";
 
 
