43ec86817ff8507d844e04d88c827fd85b8032d3
vlopez2828 <vickylopez2828@gmail.com>
2025-04-18 02:38:50 -0300

    chore(app): temporarily disable mock line

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import GlobalModal from "./shared/ui/components/GlobalModal";
 
 // 👇 Importa el hook de sockets
 import { useAvailabilitySocket } from './features/parkings/hooks/useAvailabilitySocket'; // para usar el socket real
-import { useMockAvailabilityUpdates } from './features/parkings/hooks/useMockAvailabilityUpdates' // para simular cambios sin backend
+// import { useMockAvailabilityUpdates } from './features/parkings/hooks/useMockAvailabilityUpdates' // para simular cambios sin backend
 
 
 const App = () => {
   // 👇 Usa el hook de sockets
   useAvailabilitySocket() // para usar el socket real
-  useMockAvailabilityUpdates() // para simular cambios sin backend
+  //useMockAvailabilityUpdates() // para simular cambios sin backend
 
   return (
   <ThemeProvider theme={theme}>
